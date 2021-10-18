5044331451df12077ea671ee7a2af6b760f53da7
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-18 19:02:13 -0400

    Adding information

--- components/Hero.js
+++ components/Hero.js
@@ -115,16 +115,14 @@ export default function Hero() {
 
 					<main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
 						<div className='sm:text-center lg:text-left'>
-							<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-								<span className='block xl:inline'>Data to enrich your</span>{' '}
+							<h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl flex flex-col'>
+								<span className='block xl:inline'>Kellen Wiltshire</span>
 								<span className='block text-indigo-600 xl:inline'>
-									online business
+									Web Developer
 								</span>
 							</h1>
 							<p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0'>
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
+								Kingston, Ontario, Canada
 							</p>
 							<div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start'>
 								<div className='rounded-md shadow'>
@@ -132,7 +130,7 @@ export default function Hero() {
 										href='#'
 										className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
 									>
-										Get started
+										Portfolio
 									</a>
 								</div>
 								<div className='mt-3 sm:mt-0 sm:ml-3'>
@@ -140,7 +138,7 @@ export default function Hero() {
 										href='#'
 										className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10'
 									>
-										Live demo
+										Contact Me
 									</a>
 								</div>
 							</div>
