17be99244f3efce044f61a8c2a518989f9cf88ba
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-20 16:23:44 -0400

    Starting on Projects Page

--- components/Portfolio/PortfolioCards.js
+++ components/Portfolio/PortfolioCards.js
@@ -1,24 +1,30 @@
 import React from 'react';
-
-function classNames(...classes) {
-	return classes.filter(Boolean).join(' ');
-}
+import Link from 'next/link';
 
 export default function PortfolioCards({ project }) {
 	return (
-		<a key={project.id} className='group'>
-			<div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden xl:aspect-w-8 xl:aspect-h-7'>
-				<img
-					src={project.image}
-					alt={project.description}
-					className='w-full h-full object-center object-cover group-hover:opacity-75'
-				/>
-			</div>
-			<h3 className='mt-4 text-lg text-white'>{project.name}</h3>
-			<p className=' text-xs text-gray-100'>{project.type}</p>
-			<p className='mt-1 text-sm font-medium text-gray-100'>
-				{project.description}
-			</p>
-		</a>
+		<Link
+			href={{
+				pathname: '/projects/[project]',
+				query: {
+					project: project.id,
+				},
+			}}
+		>
+			<a key={project.id} className='group'>
+				<div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden xl:aspect-w-8 xl:aspect-h-7'>
+					<img
+						src={project.image}
+						alt={project.description}
+						className='w-full h-full object-center object-cover group-hover:opacity-75'
+					/>
+				</div>
+				<h3 className='mt-4 text-lg text-white'>{project.name}</h3>
+				<p className=' text-xs text-gray-100'>{project.type}</p>
+				<p className='mt-1 text-sm font-medium text-gray-100'>
+					{project.description}
+				</p>
+			</a>
+		</Link>
 	);
 }
--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Navbar from '../../components/Layout/Navbar';
+import { projects } from '../../utils/projects';
 
-export default function project() {
+export default function project({ project, isDarkMode, setIsDarkMode }) {
+	console.log(project);
 	return (
-		<div></div>
+		<div>
+			<Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+		</div>
 	);
+}
+
+export async function getStaticPaths() {
+	const projectList = projects;
+	const projID = projectList.map((project) => {
+		return project.id;
+	});
+	const paths = projID.map((proj) => ({
+		params: {
+			project: proj,
+		},
+	}));
+	return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+	const projectID = params.project;
+	const currentProject = projects.filter((item) => item.id === projectID);
+	const project = currentProject[0];
+	return {
+		props: { project },
+	};
 }
