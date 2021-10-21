ee983bce250b33f2514757679793265ee35780b4
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-21 14:16:08 -0400

    Animation Completed

--- components/Portfolio/PortfolioCards.js
+++ components/Portfolio/PortfolioCards.js
@@ -33,26 +33,22 @@ export default function PortfolioCards({ project }) {
 				},
 			}}
 		>
-			<motion.a
-				ref={ref}
-				animate={controls}
-				variants={projectVariants}
-				key={project.id}
-				className='group'
-			>
-				<div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden xl:aspect-w-8 xl:aspect-h-7'>
-					<img
-						src={project.image}
-						alt={project.description}
-						className='w-full h-full object-center object-cover group-hover:opacity-75'
-					/>
-				</div>
-				<h3 className='mt-4 text-lg text-white'>{project.name}</h3>
-				<p className=' text-xs text-gray-100'>{project.type}</p>
-				<p className='mt-1 text-sm font-medium text-gray-100'>
-					{project.description}
-				</p>
-			</motion.a>
+			<a key={project.id} className='group'>
+				<motion.div ref={ref} animate={controls} variants={projectVariants}>
+					<div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden xl:aspect-w-8 xl:aspect-h-7'>
+						<img
+							src={project.image}
+							alt={project.description}
+							className='w-full h-full object-center object-cover group-hover:opacity-75'
+						/>
+					</div>
+					<h3 className='mt-4 text-lg text-white'>{project.name}</h3>
+					<p className=' text-xs text-gray-100'>{project.type}</p>
+					<p className='mt-1 text-sm font-medium text-gray-100'>
+						{project.description}
+					</p>
+				</motion.div>
+			</a>
 		</Link>
 	);
 }
