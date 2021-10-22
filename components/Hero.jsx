d31246ae52fc7fbb7176effa3a1f610e27b3c197
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-22 12:19:55 -0400

    File Cleanup. Made files jsx instead of js

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Navbar from './Layout/Navbar';
 
 export default function Hero({ isDarkMode, setIsDarkMode }) {
+	const [fillColour, setFillColour] = useState('#ffffff');
+	useEffect(() => {
+		isDarkMode ? setFillColour('#374151') : setFillColour('#ffffff');
+	}, [isDarkMode]);
 	return (
 		<div
 			id='hero'
@@ -9,6 +14,16 @@ export default function Hero({ isDarkMode, setIsDarkMode }) {
 		>
 			<div className='max-w-7xl mx-auto'>
 				<div className='relative z-10 pb-8 bg-white dark:bg-gray-700 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32'>
+					<svg
+						className='hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2'
+						fill={fillColour}
+						viewBox='0 0 100 100'
+						preserveAspectRatio='none'
+						aria-hidden='true'
+					>
+						<polygon points='50,0 100,0 50,100 0,100' />
+					</svg>
+
 					<Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
 
 					<main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 sm:mb-12'>
