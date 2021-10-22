130375ad996cf04c2bb75f696ed569139a37ef51
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-10-22 10:35:28 -0400

    updated SEO, Attempting to add ESLint

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
 import Footer from './Footer';
+import Header from './Header';
 
-export default function Layout({ children, title }) {
+export default function Layout({ children }) {
 	return (
 		<div id='topDiv' className='dark'>
 			<Head>
-				<title>{title}</title>
-				<link rel='icon' href='/favicon.ico' />
-				<meta name='Kellen Wiltshire Web Development Kingston Ontario Canada' />
-				<meta lang='en' />
-
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
 				<link
 					href='https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap'
 					rel='stylesheet'
 				/>
 			</Head>
+			<Header />
 			<main>{children}</main>
 			<Footer />
 		</div>
