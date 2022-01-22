c78f72025a86b84ac760c5dac72ba54f1c855192
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-01-22 11:02:25 -0500

    fix: project layout. chore: npm update

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -10,28 +10,17 @@ export default function Projects({ isDarkMode, project }) {
 	});
 	return (
 		<div id='about' className='bg-white dark:bg-gray-700 mt-2'>
-			<div>
-				<div className='max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8'>
-					<div className='max-w-3xl mx-auto space-y-2 lg:max-w-none'>
-						<h2 className='text-lg leading-6 font-semibold text-black dark:text-white uppercase tracking-wider'>
-							{project.type}
-						</h2>
-						<p className='text-3xl font-extrabold text-black dark:text-white sm:text-4xl lg:text-5xl'>
-							{project.name}
-						</p>
-					</div>
+			<div className='max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8'>
+				<div className='max-w-3xl mx-auto space-y-2 lg:max-w-none'>
+					<h1 className='text-3xl font-extrabold text-black dark:text-white sm:text-4xl lg:text-5xl'>{project.name}</h1>
 				</div>
 			</div>
-			<div className='mt-8 pb-12 bg-gray-50 dark:bg-gray-700 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24'>
+			<div className='mt-8 pb-12 bg-gray-50 dark:bg-gray-700 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-12 py-2 rounded-md'>
 				<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 					<div className='space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0'>
-						<div className='aspect-w-3 aspect-h-2 sm:aspect-w-4 sm:aspect-h-3'>
-							<img
-								className='object-cover shadow-lg rounded-lg'
-								src={project.image}
-								alt=''
-							/>
-						</div>
+						<a href={project.link} className='aspect-w-3 aspect-h-2 sm:aspect-w-4 sm:aspect-h-3 '>
+							<img className='object-cover shadow-lg rounded-lg ' src={project.image} alt='' />
+						</a>
 						<div className='sm:col-span-2'>
 							<div className='space-y-4'>
 								<div>
@@ -43,43 +32,27 @@ export default function Projects({ isDarkMode, project }) {
 									</h3>
 								</div>
 								<div className='text-lg'>
-									<p className='py-1 text-gray-500 dark:text-gray-100'>
-										{project.description}
-									</p>
+									<p className='py-1 text-gray-500 dark:text-gray-100'>{project.description}</p>
 								</div>
 								<div className='text-lg'>
-									<p className='py-1 text-gray-500 dark:text-gray-100'>
-										{project.lessons}
-									</p>
+									<p className='py-1 text-gray-500 dark:text-gray-100'>{project.lessons}</p>
 								</div>
 								<div className='text-lg'>
-									<p className='py-1 text-gray-500 underline dark:text-gray-100'>
-										Tech Stack
-									</p>
-									<p className='py-1 text-gray-500 dark:text-gray-100'>
-										Client: {project.tech.client}
-									</p>
+									<p className='py-1 text-gray-500 underline dark:text-gray-100'>Tech Stack</p>
+									<p className='py-1 text-gray-500 dark:text-gray-100'>Client: {project.tech.client}</p>
 									{project.tech.server ? (
-										<p className='py-1 text-gray-500 dark:text-gray-100'>
-											Server: {project.tech.server}
-										</p>
+										<p className='py-1 text-gray-500 dark:text-gray-100'>Server: {project.tech.server}</p>
 									) : null}
 								</div>
 								<ul role='list' className='flex space-x-5'>
 									<li>
-										<a
-											href={project.git}
-											className='text-gray-400 hover:text-gray-500'
-										>
+										<a href={project.git} className='text-gray-400 hover:text-gray-500'>
 											<span className='sr-only'>Github</span>
 											<Github colour={svgColour} />
 										</a>
 									</li>
 									<li>
-										<a
-											href={project.link}
-											className='text-gray-400 hover:text-gray-500'
-										>
+										<a href={project.link} className='text-gray-400 hover:text-gray-500'>
 											<span className='sr-only'>Website</span>
 											<Web colour={svgColour} />
 										</a>
