564b7e408c2f835e616b9f0879caceac360231c1
Kellen <webdevelopment@kellenwiltshire.com>
2022-04-25 11:01:17 -0400

    fix: styling and layout

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 
-export default function Layout({ children }) {
+export default function Layout({ children, isDarkMode }) {
+	const [colour1, setColour1] = useState('#1E3A8A');
+	const [colour2, setColour2] = useState('#09D6FE');
+
+	useEffect(() => {
+		if (isDarkMode) {
+			setColour1('#374151');
+			setColour2('');
+		} else {
+			setColour1('#1E3A8A');
+			setColour2('#09D6FE');
+		}
+	}, [isDarkMode]);
 	return (
 		<div id='topDiv' className='dark'>
-			<svg className='background--custom' id='demo' viewBox='0 0 100 100' preserveAspectRatio='none'>
+			<svg
+				className='background--custom'
+				viewBox='0 0 100 100'
+				preserveAspectRatio='none'
+			>
 				<path
-					fill='#1E3A8A'
+					fill={colour1}
 					fillOpacity={1}
 					d='M-100 -100L200 -100L200 90L-100 90Z'
-					style={{ animation: 'path0 16.129032258064516s linear infinite alternate' }}
+					style={{
+						animation: 'path0 16.129032258064516s linear infinite alternate',
+					}}
 				/>
 				<path
-					fill='#09D6FE'
+					fill={colour2}
 					fillOpacity={1}
 					d='M-100 -100L200 -100L200 40L-100 40Z'
 					style={{ animation: 'path1 12.5s linear infinite alternate' }}
 				/>
 			</svg>
+
 			<Header />
 			<main>{children}</main>
 			<Footer />
--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -15,77 +15,87 @@ const navigation = [
 
 export default function Navbar({ isDarkMode, setIsDarkMode }) {
 	return (
-		<Popover>
-			<div className='relative rounded-b-md bg-blue-900 py-2 px-4 sm:px-6 lg:px-8'>
-				<nav className='relative flex items-center justify-between sm:h-10 lg:justify-start' aria-label='Global'>
-					<div className='flex flex-shrink-0 flex-grow items-center lg:flex-grow-0'>
-						<div className='flex w-full items-center justify-between md:w-auto'>
-							<Link href='/' alt='Logo Link'>
-								<a>
-									<div>
-										<Code colour='white' />
-									</div>
-								</a>
-							</Link>
-							<div className='-mr-2 flex items-center md:hidden'>
-								<Popover.Button className=' inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-700 dark:text-white'>
-									<span className='sr-only'>Open main menu</span>
-									<MenuIcon className='h-6 w-6' aria-hidden='true' />
-								</Popover.Button>
+		<div className='flex min-w-full flex-row justify-center'>
+			<Popover>
+				<div className='relative rounded-b-md bg-blue-900 py-2 px-4 sm:px-6 lg:px-8'>
+					<nav
+						className='relative flex items-center justify-between sm:h-10 lg:justify-start'
+						aria-label='Global'
+					>
+						<div className='flex flex-shrink-0 flex-grow items-center lg:flex-grow-0'>
+							<div className='flex w-full items-center justify-between md:w-auto'>
+								<Link href='/' alt='Logo Link'>
+									<a>
+										<div>
+											<Code colour='white' />
+										</div>
+									</a>
+								</Link>
+								<div className='-mr-2 flex items-center md:hidden'>
+									<Popover.Button className=' inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-700 dark:text-white'>
+										<span className='sr-only'>Open main menu</span>
+										<MenuIcon className='h-6 w-6' aria-hidden='true' />
+									</Popover.Button>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className='hidden md:ml-10 md:block md:space-x-8 md:pr-4'>
-						{navigation.map((item) => (
-							<Link key={item.name} href={item.href}>
-								<a className='font-medium text-white  hover:text-gray-300'>{item.name}</a>
-							</Link>
-						))}
-						<Toggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-					</div>
-				</nav>
-			</div>
-
-			<Transition
-				as={Fragment}
-				enter='duration-150 ease-out'
-				enterFrom='opacity-0 scale-95'
-				enterTo='opacity-100 scale-100'
-				leave='duration-100 ease-in'
-				leaveFrom='opacity-100 scale-100'
-				leaveTo='opacity-0 scale-95'
-			>
-				<Popover.Panel
-					focus
-					className='absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden'
-				>
-					<div className='overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5 dark:bg-gray-600'>
-						<div className='flex items-center justify-between px-5 pt-4'>
-							<div>
-								<Code colour='black' />
-							</div>
-							<div className='-mr-2'>
-								<Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-500 dark:text-white'>
-									<span className='sr-only'>Close main menu</span>
-									<XIcon className='h-6 w-6' aria-hidden='true' />
-								</Popover.Button>
-							</div>
-						</div>
-						<div className='space-y-1 px-2 pt-2 pb-3'>
+						<div className='hidden md:ml-10 md:block md:space-x-8 md:pr-4'>
 							{navigation.map((item) => (
 								<Link key={item.name} href={item.href}>
-									<a className='block rounded-md px-3 py-2 text-base font-medium text-black  hover:bg-gray-50 hover:text-gray-900 '>
+									<a className='font-medium text-white  hover:text-gray-300'>
 										{item.name}
 									</a>
 								</Link>
 							))}
+							<Toggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+						</div>
+					</nav>
+				</div>
+
+				<Transition
+					as={Fragment}
+					enter='duration-150 ease-out'
+					enterFrom='opacity-0 scale-95'
+					enterTo='opacity-100 scale-100'
+					leave='duration-100 ease-in'
+					leaveFrom='opacity-100 scale-100'
+					leaveTo='opacity-0 scale-95'
+				>
+					<Popover.Panel
+						focus
+						className='absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden'
+					>
+						<div className='overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5 dark:bg-gray-600'>
+							<div className='flex items-center justify-between px-5 pt-4'>
+								<div>
+									<Code colour='black' />
+								</div>
+								<div className='-mr-2'>
+									<Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-500 dark:text-white'>
+										<span className='sr-only'>Close main menu</span>
+										<XIcon className='h-6 w-6' aria-hidden='true' />
+									</Popover.Button>
+								</div>
+							</div>
 							<div className='space-y-1 px-2 pt-2 pb-3'>
-								<Toggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+								{navigation.map((item) => (
+									<Link key={item.name} href={item.href}>
+										<a className='block rounded-md px-3 py-2 text-base font-medium text-black  hover:bg-gray-50 hover:text-gray-900 '>
+											{item.name}
+										</a>
+									</Link>
+								))}
+								<div className='space-y-1 px-2 pt-2 pb-3'>
+									<Toggle
+										isDarkMode={isDarkMode}
+										setIsDarkMode={setIsDarkMode}
+									/>
+								</div>
 							</div>
 						</div>
-					</div>
-				</Popover.Panel>
-			</Transition>
-		</Popover>
+					</Popover.Panel>
+				</Transition>
+			</Popover>
+		</div>
 	);
 }
